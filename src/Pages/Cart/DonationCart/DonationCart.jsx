0bb1e62c9485a3447a981b7b7a0276659fd2e3b2
khokons <khokon.web9226@gmail.com>
2023-09-26 13:45:22 +0600

    local storage part done

--- src/Pages/Cart/DonationCart/DonationCart.jsx
+++ src/Pages/Cart/DonationCart/DonationCart.jsx
@@ -1,4 +1,8 @@
+/* eslint-disable no-undef */
 /* eslint-disable react/prop-types */
+
+import swal from "sweetalert";
+
 
 
 const DonationCart = ({cart}) => {
@@ -6,13 +10,35 @@ const DonationCart = ({cart}) => {
     const {Picture, Description, Title, Price,Text_button_bg } =
     cart || {};
 
+    const handleDonationBtn= () =>{
+        const addedDonation = [];
+        const donationItems = JSON.parse(localStorage.getItem('favorites'))
+
+        if(!donationItems){
+            addedDonation.push(cart)
+            localStorage.setItem('favorites',JSON.stringify(addedDonation))
+            swal("Donation Added Successful!", "You clicked the button!", "success");
+        }
+
+
+        else{
+                addedDonation.push(... donationItems, cart)
+                localStorage.setItem('favorites',JSON.stringify(addedDonation))
+                swal("Donation Added Successful!", "You clicked the button!", "success"); 
+
+        }
+
+    }
+
     return (
         
      <div>
         <div className="card max-w-screen-xl h-[350px] mx-auto bg-gray-700">
         <figure><img className="w-full"  src={Picture} alt="Shoes" />
         <div className="absolute h-[85px] w-full bg-gray-950 opacity-60 bottom-0 left-0"></div>
-      <button style={{backgroundColor: Text_button_bg}} className="px-2 py-2 absolute left-6 bottom-5 text-white">Donate {Price}</button>
+      <button onClick={handleDonationBtn} style={{backgroundColor: Text_button_bg}} className="px-2 py-2 absolute left-6 bottom-5 text-white">
+        Donate {Price}
+        </button>
         </figure>
       </div>
 
@@ -20,6 +46,8 @@ const DonationCart = ({cart}) => {
         <h2 className="text-4xl font-bold text-[#0B0B0B] py-6">{Title}</h2>
         <p>{Description}</p>
       </div>
+
+ 
      </div>
       
 
--- src/Pages/Donation/Donation.jsx
+++ src/Pages/Donation/Donation.jsx
@@ -1,10 +1,38 @@
+import { useEffect, useState } from "react";
+import DonationCart from "../Cart/DonationCart/DonationCart";
+import Donationpage from "../DonationPage/Donationpage";
 
 const Donation = () => {
+    const [donations, setDonations] = useState([]);
+    const [notFound, setNotFound] = useState(false);
+
+    useEffect(() => {
+        const donationItems = JSON.parse(localStorage.getItem('favorites'));
+        if(donationItems){
+            setDonations(donationItems);
+        }
+        else{
+            setNotFound("No Data Found");
+        }
+    },[])
+
+
     return (
         <div>
-            <h2>No Data Found</h2>
+          {
+            notFound? <p className="flex justify-center items-center h-[300px] text-4xl text-black">{notFound}</p> 
+            : 
+            <div>
+                <div className="grid md:grid-cols-2 gap-6">
+                    {
+donations.map(cart => <Donationpage key={cart.id} cart={cart}></Donationpage>)
+                    }
+                </div>
+            </div>
+          }
         </div>
     );
 };
 
 export default Donation;
+
